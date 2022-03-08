f6fda00097f1a20a37057dec94bafc652c33b1dd
ashirakvateq <ashir@akvateq.com>
2022-03-08 06:55:05 +0500

    countries seeding file added - incomplete list.

---
+++ database/seeders/CountriesSeeder.php
@@ -0,0 +1,335 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Country;
+use Illuminate\Database\Seeder;
+
+class CountriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Countires seeding
+
+        //UK 
+        $country = new Country();
+        $country->name = 'UK';
+        $country->phone_code = '44';
+        $country->save();
+
+        //USA  
+        $country = new Country();
+        $country->name = 'USA ';
+        $country->phone_code = '1';
+        $country->save();
+
+        //Pakistan
+        $country = new Country();
+        $country->name = 'Pakistan';
+        $country->phone_code = '92';
+        $country->save();
+
+        //Algeria
+        $country = new Country();
+        $country->name = 'Algeria';
+        $country->phone_code = '376';
+        $country->save();
+
+        //Andorra
+        $country = new Country();
+        $country->name = 'Andorra';
+        $country->phone_code = '244';
+        $country->save();
+
+
+        //Anguilla 
+        $country = new Country();
+        $country->name = 'Anguilla';
+        $country->phone_code = '1264';
+        $country->save();
+
+
+        //Antigua
+        $country = new Country();
+        $country->name = 'Antigua';
+        $country->phone_code = '1268';
+        $country->save();
+
+
+        //Argentina 
+        $country = new Country();
+        $country->name = 'Argentina';
+        $country->phone_code = '54';
+        $country->save();
+
+
+        //Armenia 
+        $country = new Country();
+        $country->name = 'Armenia';
+        $country->phone_code = '374';
+        $country->save();
+
+
+        //Aruba 
+        $country = new Country();
+        $country->name = 'Aruba';
+        $country->phone_code = '297';
+        $country->save();
+
+
+        //Australia 
+        $country = new Country();
+        $country->name = 'Australia';
+        $country->phone_code = '61';
+        $country->save();
+
+
+        //Austria 
+        $country = new Country();
+        $country->name = 'Austria';
+        $country->phone_code = '43';
+        $country->save();
+
+        //Azerbaijan
+        $country = new Country();
+        $country->name = 'Azerbaijan';
+        $country->phone_code = '994';
+        $country->save();
+
+        //Bahamas 
+        $country = new Country();
+        $country->name = 'Bahamas';
+        $country->phone_code = '1242';
+        $country->save();
+
+        //Bahrain 
+        $country = new Country();
+        $country->name = 'Bahrain';
+        $country->phone_code = '973';
+        $country->save();
+
+        //Bangladesh 
+        $country = new Country();
+        $country->name = 'Bangladesh';
+        $country->phone_code = '880';
+        $country->save();
+
+        //Barbados 
+        $country = new Country();
+        $country->name = 'Barbados';
+        $country->phone_code = '1246';
+        $country->save();
+
+        //Belarus 
+        $country = new Country();
+        $country->name = 'Belarus';
+        $country->phone_code = '375';
+        $country->save();
+
+        //Belgium  
+        $country = new Country();
+        $country->name = 'Belgium';
+        $country->phone_code = '32';
+        $country->save();
+
+        //Belize  
+        $country = new Country();
+        $country->name = 'Belize';
+        $country->phone_code = '501';
+        $country->save();
+
+        //Benin  
+        $country = new Country();
+        $country->name = 'Benin';
+        $country->phone_code = '229';
+        $country->save();
+
+        //Bermuda   
+        $country = new Country();
+        $country->name = 'Bermuda';
+        $country->phone_code = '1441';
+        $country->save();
+
+        //Bhutan   
+        $country = new Country();
+        $country->name = 'Bhutan ';
+        $country->phone_code = '975';
+        $country->save();
+
+        //Bolivia   
+        $country = new Country();
+        $country->name = 'Bolivia';
+        $country->phone_code = '591';
+        $country->save();
+
+        //Bosnia Herzegovina  
+        $country = new Country();
+        $country->name = 'Bosnia Herzegovina';
+        $country->phone_code = '387';
+        $country->save();
+
+        //Botswana 
+        $country = new Country();
+        $country->name = 'Botswana';
+        $country->phone_code = '267';
+        $country->save();
+
+        //Brazil 
+        $country = new Country();
+        $country->name = 'Brazil';
+        $country->phone_code = '55';
+        $country->save();
+
+        //Brunei  
+        $country = new Country();
+        $country->name = 'Brunei';
+        $country->phone_code = '673';
+        $country->save();
+
+        //Bulgaria 
+        $country = new Country();
+        $country->name = 'Bulgaria';
+        $country->phone_code = '359';
+        $country->save();
+
+        //Burkina Faso 
+        $country = new Country();
+        $country->name = 'Burkina Faso';
+        $country->phone_code = '226';
+        $country->save();
+
+        //Burundi  
+        $country = new Country();
+        $country->name = 'Burundi';
+        $country->phone_code = '257';
+        $country->save();
+
+        //Cambodia 
+        $country = new Country();
+        $country->name = 'Cambodia';
+        $country->phone_code = '855';
+        $country->save();
+
+        //Cameroon 
+        $country = new Country();
+        $country->name = 'Cameroon';
+        $country->phone_code = '237';
+        $country->save();
+
+        //Canada  
+        $country = new Country();
+        $country->name = 'Canada';
+        $country->phone_code = '1';
+        $country->save();
+
+
+        //Cape Verde Islands  
+        $country = new Country();
+        $country->name = 'Cape Verde Islands';
+        $country->phone_code = '238';
+        $country->save();
+
+        //Cayman Islands  
+        $country = new Country();
+        $country->name = 'Cayman Islands';
+        $country->phone_code = '1345';
+        $country->save();
+
+        //Central African Republic  
+        $country = new Country();
+        $country->name = 'Central African Republic';
+        $country->phone_code = '236';
+        $country->save();
+
+        //Chile  
+        $country = new Country();
+        $country->name = 'Chile';
+        $country->phone_code = '56';
+        $country->save();
+
+        //China   
+        $country = new Country();
+        $country->name = 'China';
+        $country->phone_code = '86';
+        $country->save();
+
+        //Colombia    
+        $country = new Country();
+        $country->name = 'Colombia';
+        $country->phone_code = '57';
+        $country->save();
+
+        //Comoros    
+        $country = new Country();
+        $country->name = 'Comoros';
+        $country->phone_code = '269';
+        $country->save();
+
+        //Congo    
+        $country = new Country();
+        $country->name = 'Congo';
+        $country->phone_code = '242';
+        $country->save();
+
+        //Cook Islands   
+        $country = new Country();
+        $country->name = 'Cook Islands';
+        $country->phone_code = '682';
+        $country->save();
+
+        //Costa Rica   
+        $country = new Country();
+        $country->name = 'Costa Rica';
+        $country->phone_code = '506';
+        $country->save();
+
+        //Croatia    
+        $country = new Country();
+        $country->name = 'Croatia';
+        $country->phone_code = '385';
+        $country->save();
+
+        //Cuba    
+        $country = new Country();
+        $country->name = 'Cuba';
+        $country->phone_code = '53';
+        $country->save();
+
+        //Cyprus North   
+        $country = new Country();
+        $country->name = 'Cyprus North';
+        $country->phone_code = '90392';
+        $country->save();
+
+        //Cyprus South 
+        $country = new Country();
+        $country->name = 'Cyprus South';
+        $country->phone_code = '357';
+        $country->save();
+
+        //Czech Republic   
+        $country = new Country();
+        $country->name = 'Czech Republic';
+        $country->phone_code = '42';
+        $country->save();
+
+        //Denmark    
+        $country = new Country();
+        $country->name = 'Denmark';
+        $country->phone_code = '45';
+        $country->save();
+
+        //Djibouti  
+        $country = new Country();
+        $country->name = 'Djibouti';
+        $country->phone_code = '253';
+        $country->save();
+
+
+    }
+}
